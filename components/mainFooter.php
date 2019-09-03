cfb41987fd28978f3a8cb7fa4c338006592d927d
Mantas Baltakis <mantas.baltakis@gmail.com>
2019-09-03 16:24:26 +0100

    Refactored footer

---
+++ components/mainFooter.php
@@ -0,0 +1,23 @@
+<footer>
+    <p>Find Us on social media</p>
+    <!-- H1, Header background selection menu -->
+    <select name="font-selection" class="font-selection">
+        <option>Font Size</option>
+        <option value="Default">Normal</option>
+        <option value="Small">Small</option>
+        <option value="Medium">Medium</option>
+        <option value="Large">Large</option>
+    </select>
+    <select name="background-selection" class="background-selection">
+        <option>Header color</option>
+        <option value="Default">Transparent</option>
+        <option value="Red">Background: Red</option>
+        <option value="Blue">Background: Blue</option>
+        <option value="Green">Background: Green</option>
+        <option value="Yellow">Background: Yellow</option>
+    </select>
+    <button class="footer-a1 btn" onmousedown="toggleOverlay()">Subscribe</button>
+    <a class="footer-a2" href="https://www.facebook.com" target="_blank"><img src="assets/facebook.png" alt=""></a>
+    <a class="footer-a3" href="https://www.instagram.com" target="_blank"><img src="assets/instagram.png" alt=""></a>
+    <a class="footer-a4" href="https://www.twitter.com" target="_blank"><img src="assets/twitter.png" alt=""></a>
+</footer>
--- views/home.php
+++ views/home.php
@@ -22,7 +22,7 @@
                 <div class="content-wrapper">
                     <h1>Welcome to GYMBRO</h1>
                     <p>All purpose highest quality gym equipment</p>
-                    <a class="btn btn-main" href="#first-card">Shop Now</a>
+                    <a class="btn btn-main" href="#main-cards">Shop Now</a>
                 </div>
             </section>
         </header>
@@ -89,29 +89,8 @@
                 <button onmousedown="toggleOverlay()" class="btn form-close">Close Form</button>
             </form>
         </div>
-        <footer>
-            <p>Find Us on social media</p>
-            <!-- H1, Header background selection menu -->
-            <select name="font-selection" class="font-selection">
-                <option>Font Size</option>
-                <option value="Default">Normal</option>
-                <option value="Small">Small</option>
-                <option value="Medium">Medium</option>
-                <option value="Large">Large</option>
-            </select>
-            <select name="background-selection" class="background-selection">
-                <option>Header color</option>
-                <option value="Default">Transparent</option>
-                <option value="Red">Background: Red</option>
-                <option value="Blue">Background: Blue</option>
-                <option value="Green">Background: Green</option>
-                <option value="Yellow">Background: Yellow</option>
-            </select>
-            <button href="#video-section" class="footer-a1 btn" onmousedown="toggleOverlay()">Subscribe</button>
-            <a class="footer-a2" href="https://www.facebook.com" target="_blank"><img src="assets/facebook.png" alt=""></a>
-            <a class="footer-a3" href="https://www.instagram.com" target="_blank"><img src="assets/instagram.png" alt=""></a>
-            <a class="footer-a4" href="https://www.twitter.com" target="_blank"><img src="assets/twitter.png" alt=""></a>
-        </footer>
+        <?php include('components/mainFooter.php') ?>
+
     </main>
 </body>
 <script src="js/script.js"></script>
